a5259fce2090405fa7b3daded345b57df83cb842
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-31 11:49:19 -0600

    Add add test for item instance method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,8 +12,18 @@ RSpec.describe Item, type: :model do
       it 'should return the supermarket name of the item' do
         @supermarket_1 = Supermarket.create!(name: 'Mountain Market', location: '123 Cherry St.')
         @item_1 = @supermarket_1.items.create!(name: 'Grapes', price: 4)
-        
+
         expect(@item_1.supermarket_name).to eq('Mountain Market')
+      end
+    end
+    describe '.customer_count' do
+      it 'should return the number of customers that have bought the item' do
+        @supermarket_1 = Supermarket.create!(name: 'Mountain Market', location: '123 Cherry St.')
+        @item_1 = @supermarket_1.items.create!(name: 'Grapes', price: 4)
+        @customer_1 = @item_1.customers.create!(name: 'John Doe')
+        @customer_2 = @item_1.customers.create!(name: 'Sally Smith')
+        
+        expect(@item_1.customer_count).to eq(2)
       end
     end
   end
