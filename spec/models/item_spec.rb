7dfb7810314610ccdd0e6c0e8174f6b74d54d860
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-31 10:36:31 -0600

    Add test for item instance method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Item, type: :model do
     it { should have_many :customer_items}
     it { should have_many(:customers).through(:customer_items)}
   end
+
+  describe 'instance methods' do
+    describe '.supermarket_name' do
+      it 'should return the supermarket name of the item' do
+        @supermarket_1 = Supermarket.create!(name: 'Mountain Market', location: '123 Cherry St.')
+        @item_1 = @supermarket_1.items.create!(name: 'Grapes', price: 4)
+        
+        expect(@item_1.supermarket_name).to eq('Mountain Market')
+      end
+    end
+  end
 end
